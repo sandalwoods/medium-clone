05e2422a96836c566b58791df9ec679a9d70752b
sandalwoods <1764076874@qq.com>
2022-11-09 17:07:57 +0800

    front end done

--- components/Recommendatons.js
+++ components/Recommendatons.js
@@ -20,14 +20,17 @@ const styles = {
   authorFollowing: `text-[#787878]`,
   authorActions: `flex gap-[.6rem] my-[1rem]`,
   actionButton: `bg-[#1A8917] text-white rounded-full px-[.6rem] py-[.4rem] text-sm`,
+  recommendationContainer: ``,
+  title: `my-[1rem]`,
+  articlesContainer: ``,
+  articleContentWrapper: `flex items-center justify-between cursor-pointer my-[1rem]`,
+  articleContent: `flex-[4]`,
+  recommendationAuthorContainer: `flex items-center gap-[.6rem]`,
   recommendationAuthorProfileImageContainer: `rounded-full overflow-hidden h-[1.4rem] w-[1.4rem]`,
   recommendationAuthorName: `text-sm`,
-  recommendationAuthorContainer: `flex items-center gap-[.6rem]`,
   recommendationTitle: `font-bold`,
   recommendationThumbnailContainer: `flex flex-1 items-center justify-center h-[4rem] w-[4rem]`,
   recommendationThumbnail: `object-cover`,
-  articleContentWrapper: `flex items-center justify-between cursor-pointer my-[1rem]`,
-  articleContent: `flex-[4]`,
 };
 
 const Recommendatons = () => {
@@ -58,31 +61,33 @@ const Recommendatons = () => {
       <div className={styles.recommendationContainer}>
         <div className={styles.title}>More from Medium</div>
         <div className={styles.articlesContainer}>
-          <div className={styles.articleContentWrapper}>
-            <div className={styles.articleContent}>
-              <div className={styles.recommendationAuthorContainer}>
-                <div
-                  className={styles.recommendationAuthorProfileImageContainer}
-                >
-                  <Image src={Qazi} width={100} height={100} />
+          {recommendedPosts.map((post) => (
+            <div className={styles.articleContentWrapper}>
+              <div className={styles.articleContent}>
+                <div className={styles.recommendationAuthorContainer}>
+                  <div
+                    className={styles.recommendationAuthorProfileImageContainer}
+                  >
+                    <Image src={post.author.image} width={100} height={100} />
+                  </div>
+                  <div className={styles.recommendationAuthorName}>
+                    {post.author.name}
+                  </div>
                 </div>
-                <div className={styles.recommendationAuthorName}>
-                  Rafeh Qazi
+                <div className={styles.recommendationTitle}>
+                  {post.title}
                 </div>
               </div>
-              <div className={styles.recommendationTitle}>
-                The Ultimate Javascript fro Beginners by clever programmer.
+              <div className={styles.recommendationThumbnailContainer}>
+                <Image
+                  className={styles.recommendationThumbnail}
+                  src={post.image}
+                  width={100}
+                  height={100}
+                />
               </div>
             </div>
-            <div className={styles.recommendationThumbnailContainer}>
-              <Image
-                className={styles.recommendationThumbnail}
-                src={JSLogo}
-                width={100}
-                height={100}
-              />
-            </div>
-          </div>
+          ))}
         </div>
       </div>
     </div>
@@ -90,3 +95,30 @@ const Recommendatons = () => {
 };
 
 export default Recommendatons;
+
+const recommendedPosts = [
+  {
+    title: "What can you do with Replit?",
+    image: ReplitLogo,
+    author: {
+      name: "Clever Programmer",
+      image: CPLogo,
+    },
+  },
+  {
+    title: "The Ultimate Javascript fro Beginners by clever programmer",
+    image: TutorialImg,
+    author: {
+      name: "Rafeh Qazi",
+      image: Qazi,
+    },
+  },
+  {
+    title: "How to Become a Developer in 2022?",
+    image: JSLogo,
+    author: {
+      name: "Clever Programmer",
+      image: CPLogo,
+    },
+  },
+];
